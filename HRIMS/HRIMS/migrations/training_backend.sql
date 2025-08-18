f5321d32699372561d3547bd902bedb7e19bb071
Baraka Mkumbwa <barakamkumbwa106@gmail.com>
2025-08-18 08:28:07 +0300

    updated training_backend.sql

--- HRIMS/HRIMS/migrations/training_backend.sql
+++ HRIMS/HRIMS/migrations/training_backend.sql
@@ -27,8 +27,8 @@ ALTER TABLE public.position OWNER TO postgres;
 -- ========================================
 CREATE TABLE IF NOT EXISTS public.application (
     id SERIAL PRIMARY KEY,
-    applicant_id INTEGER REFERENCES public.users(id),
-    job_id INTEGER REFERENCES public.job_advert(id),
+    applicant_id INTEGER ,
+    job_id INTEGER ,
     resume TEXT,
     status VARCHAR(20) DEFAULT 'Pending',
     resvd5 CHAR(1),
@@ -50,12 +50,12 @@ ALTER TABLE public.application OWNER TO postgres;
 -- ========================================
 CREATE TABLE IF NOT EXISTS public.approval (
     id SERIAL PRIMARY KEY,
-    request_id INTEGER REFERENCES public.requests(id) ON DELETE CASCADE,
-    approved_by INTEGER REFERENCES public.users(id),
+    request_id INTEGER,
+    approved_by INTEGER ,
     status VARCHAR(20) NOT NULL,
     comment TEXT,
-    approved_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    created_by INTEGER REFERENCES public.officers(id) ON DELETE SET NULL,
+    approved_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_by INTEGER,
     resvd5 CHAR(1),
     resvd4 CHAR(1),
     resvd3 CHAR(1),
@@ -72,7 +72,7 @@ ALTER TABLE public.approval OWNER TO postgres;
 -- ========================================
 CREATE TABLE IF NOT EXISTS public.audit_logs (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES public.users(id) ON DELETE SET NULL,
+    user_id INTEGER ,
     action VARCHAR(100) NOT NULL,
     table_name VARCHAR(50),
     record_id INTEGER,
@@ -95,8 +95,8 @@ ALTER TABLE public.audit_logs OWNER TO postgres;
 -- ========================================
 CREATE TABLE IF NOT EXISTS public.budget (
     budget_id SERIAL PRIMARY KEY,
-    request_id INTEGER NOT NULL REFERENCES public.requests(id) ON DELETE CASCADE,
-    salary_scale_id INTEGER NOT NULL REFERENCES public.salary_scale(id) ON DELETE CASCADE,
+    request_id INTEGER NOT NULL ,
+    salary_scale_id INTEGER NOT NULL,
     number_of_officers INTEGER NOT NULL,
     total_budget NUMERIC(15,2) NOT NULL,
     description TEXT,
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS public.campus (
     campus_id SERIAL PRIMARY KEY,
     campus_name VARCHAR(255) NOT NULL,
     description TEXT,
-    department_id INTEGER REFERENCES public.departments(id) ON DELETE SET NULL,
+    department_id INTEGER,
     resvd5 CHAR(1),
     resvd4 CHAR(1),
     resvd3 CHAR(1),
