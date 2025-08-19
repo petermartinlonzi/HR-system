587b0de28bba6efcb6ed8b9ff0e3f946c6b8cf0d
Rehema Hasani <rehemarashidi243@gmail.com>
2025-08-19 01:16:14 -0700

    changes for officer_roles

---
+++ HRIMS/HRIMS/services/entity/officer_roles.go
@@ -0,0 +1,62 @@
+package entity
+
+import (
+	"errors"
+	"time"
+	"training-backend/package/log"
+)
+
+type Officer_roles struct {
+	ID          int32
+	RoleName    string
+	Description string
+	CreatedBy   int32
+	UpdatedBy   int32
+	DeletedBy   int32
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   time.Time
+}
+
+// Constructor kwa OfficerRole mpya
+func NewOfficerRole(roleName, description string, createdBy int32) (*OfficerRole, error) {
+	entity := &OfficerRole{
+		RoleName:    roleName,
+		Description: description,
+		CreatedBy:   createdBy,
+		CreatedAt:   time.Now(),
+	}
+
+	err := entity.ValidateNewOfficerRole()
+	if err != nil {
+		log.Errorf("error validating new OfficerRole entity %v", err)
+		return &OfficerRole{}, err
+	}
+
+	return entity, nil
+}
+
+// Validation wakati wa ku-create
+func (r *OfficerRole) ValidateNewOfficerRole() error {
+	if r.RoleName == "" {
+		return errors.New("error validating OfficerRole entity, roleName field required")
+	}
+	if r.CreatedBy <= 0 {
+		return errors.New("error validating OfficerRole entity, createdBy field required")
+	}
+	return nil
+}
+
+// Validation wakati wa ku-update
+func (r *OfficerRole) ValidateUpdateOfficerRole() error {
+	if r.ID <= 0 {
+		return errors.New("error validating OfficerRole entity, id field required")
+	}
+	if r.RoleName == "" {
+		return errors.New("error validating OfficerRole entity, roleName field required")
+	}
+	if r.UpdatedBy <= 0 {
+		return errors.New("error validating OfficerRole entity, updatedBy field required")
+	}
+	return nil
+}
